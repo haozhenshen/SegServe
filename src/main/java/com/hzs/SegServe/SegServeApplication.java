64e51b4eb93666911804759abd4fab7cdb78b050
haozhenshen <haozhen.shen@gmail.com>
2023-10-18 22:27:14 -0400

    first commit

---
+++ src/main/java/com/hzs/SegServe/SegServeApplication.java
@@ -0,0 +1,13 @@
+package com.hzs.SegServe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegServeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegServeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hzs/SegServe/SegServeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hzs.SegServe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegServeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
